ace6488843d1ca820fad76eaac586104d79afc42
ArdhraVS <ardhra139@gmail.com>
2026-09-20 17:07:46 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pickle
+
+with open("loan_approval_decision_tree.pkl", "rb") as file:
+    model = pickle.load(file)
+
+st.title("Loan Approval Prediction")
+st.subheader("Decision Tree")
+
+income = st.number_input("Income", min_value=0.0)
+credit_score = st.number_input("Credit Score", min_value=0.0)
+
+if st.button("Predict Loan Approval"):
+
+    prediction = model.predict([[income, credit_score]])
+
+    if prediction[0] == 1:
+        st.success("Loan Approved")
+    else:
+        st.error("Loan Not Approved")
